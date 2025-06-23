aedbd96f16919e8ddc5a7691914611a283f0cab9
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2025-06-23 19:05:30 -0400

    ✨ Implement Get Book Copy endpoint

---
+++ src/MacheteBang.BookLending.Books/Endpoints/GetBookCopyEndpoint.cs
@@ -0,0 +1,39 @@
+namespace MacheteBang.BookLending.Books.Endpoints;
+
+internal sealed class GetBookCopyEndpoint : IBooksEndpoint
+{
+    public void MapBooksEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapBookCopiesGroup()
+            .MapGet("{copyId}", async ([FromRoute] Guid id, [FromRoute] Guid copyId, [FromServices] BooksDbContext booksDb) =>
+            {
+                ErrorOr<BookCopy> bookCopy = await GetBookCopyAsync(id, copyId, booksDb);
+
+                return bookCopy.Match(
+                    bookCopy => Results.Ok(bookCopy.ToResponse()),
+                    errors => errors.First().Type switch
+                    {
+                        ErrorType.NotFound => Results.NotFound($"Book copy with ID {copyId} not found."),
+                        _ => Results.Problem(errors.First().Description)
+                    });
+            })
+            .Produces<BookCopyResponse>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status404NotFound)
+            .WithDescription("Gets a specific copy of a book by its ID")
+            .WithName("GetBookCopy")
+            .WithSummary("Get a Book Copy by ID");
+    }
+
+    private static async Task<ErrorOr<BookCopy>> GetBookCopyAsync(Guid bookId, Guid copyId, BooksDbContext booksDb)
+    {
+        BookCopy? bookCopy = await booksDb.BookCopies
+            .FirstOrDefaultAsync(bc => bc.BookId == bookId && bc.BookCopyId == copyId);
+
+        if (bookCopy is null)
+        {
+            return Error.NotFound();
+        }
+
+        return bookCopy;
+    }
+}
