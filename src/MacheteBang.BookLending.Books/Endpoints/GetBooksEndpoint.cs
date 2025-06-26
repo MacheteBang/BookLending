cc02a78701f5871fdb2218d3bc1f9710e815b9b3
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2025-06-26 08:17:16 -0400

    ✨ Enhance GetBooks endpoint to support query parameter for filtering books

--- src/MacheteBang.BookLending.Books/Endpoints/GetBooksEndpoint.cs
+++ src/MacheteBang.BookLending.Books/Endpoints/GetBooksEndpoint.cs
@@ -5,9 +5,9 @@ internal sealed class GetBooksEndpoint : IBooksEndpoint
     public void MapBooksEndpoint(IEndpointRouteBuilder app)
     {
         app.MapBooksGroup()
-            .MapGet(string.Empty, async ([FromServices] BooksDbContext booksDb) =>
+            .MapGet(string.Empty, async ([FromServices] BooksDbContext booksDb, [FromQuery] string? q) =>
             {
-                ICollection<Book> books = await GetAllBooksAsync(booksDb);
+                ICollection<Book> books = await GetAllBooksAsync(booksDb, q);
 
                 return Results.Ok(books.ToResponse());
             })
@@ -17,11 +17,18 @@ internal sealed class GetBooksEndpoint : IBooksEndpoint
             .WithSummary("Get All Books");
     }
 
-    private static async Task<ICollection<Book>> GetAllBooksAsync(BooksDbContext booksDb)
+    private static async Task<ICollection<Book>> GetAllBooksAsync(BooksDbContext booksDb, string? query = null)
     {
-        // TODO: Should this be a better return type?
-        return await booksDb.Books
+        var booksQuery = booksDb.Books
             .Include(b => b.Copies)
-            .ToArrayAsync();
+            .AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(query))
+        {
+            booksQuery = booksQuery.Where(b => b.Title.Contains(query) || b.Author.Contains(query));
+        }
+
+        // TODO: Should this be a better return type?
+        return await booksQuery.ToArrayAsync();
     }
 }
